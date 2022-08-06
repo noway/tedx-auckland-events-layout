47927ece4a28f4cb960c15fb183137466e7e1d4f
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-08-06 16:46:57 +1200

    implement react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { Link, Route, Routes } from "react-router-dom";
 import Demo from "./Demo";
 import Tests from "./Tests";
 
 export default function App() {
   return (
     <>
-      {/* <Demo /> */}
-      <Tests />
+      <Link to="/demo">Demo</Link> | <Link to="/tests">Tests</Link>
+      <Routes>
+        <Route path="demo" element={<Demo />} />
+        <Route path="tests" element={<Tests />} />
+      </Routes>
     </>
   );
 }
