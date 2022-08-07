d42a521356faebbe2a57274c48a43c6c744fd05d
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-08-07 15:02:35 +1200

    add some padding

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ export default function App() {
   return (
     <>
       <Link to="/demo">Demo</Link> | <Link to="/tests">Tests</Link>
+      <br/>
+      <br/>
       <Routes>
         <Route path="demo" element={<Demo />} />
         <Route path="tests" element={<Tests />} />
